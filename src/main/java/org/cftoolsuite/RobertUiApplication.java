72d9be84d0f5642107b816e1c45f3a6ff8921e02
Chris Phillipson <cphillipson@pivotal.io>
2024-08-27 06:50:45 -0700

    Switch Vaadin theme to Lumo.LIGHT

--- src/main/java/org/cftoolsuite/RobertUiApplication.java
+++ src/main/java/org/cftoolsuite/RobertUiApplication.java
@@ -7,11 +7,11 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
-import com.vaadin.flow.theme.material.Material;
+import com.vaadin.flow.theme.lumo.Lumo;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-@Theme(themeClass = Material.class, variant = Material.DARK)
+@Theme(themeClass = Lumo.class, variant = Lumo.LIGHT)
 @PWA(name = "A simple user interface to interact with a robert instance", shortName = "robert-ui")
 public class RobertUiApplication implements AppShellConfigurator {
 
